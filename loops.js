38546f63c12e2292043888ddd39b7cd93fa75206
Jared Burnham <zlolris97@gmail.com>
2019-06-20 22:37:23 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,3 +15,28 @@ function forLoop(array){
   console.log(array[1])
   return array
 }
+function forLoop(array){
+  for (let i = 0; i < 25; i++){
+     if (i===1){
+       array[i]= `I am ${i} strange loop.`
+     }
+  
+      else if(i==24){
+       array[i]=`I am ${i} strange loops.`
+  
+       }
+    else{
+      array[i]=`I am ${i} strange loops.`
+      }
+    }
+  console.log(array[1])
+  return array
+}
+
+//FUNCTION WHILELOOP(N){
+//  WHILE(){
+    CONSOLE.LOG(N)
+  
+  }
+  RETURN 'DONE'
+}
